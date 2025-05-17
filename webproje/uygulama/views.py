003fbc1bdf2e44fadd61f31049f00095f03884d3
Simsekomer <omer20200@hotmail.com>
2025-05-17 19:18:40 +0300

    feat(oop): Implement vehicle management views and forms

--- webproje/uygulama/views.py
+++ webproje/uygulama/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from .forms import AracForm, MusteriForm
+from .models import Arac, Musteri
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
-# Create your views here.
+
+@login_required
+def admin_arac_liste(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    
+    araclar_list = Arac.objects.all()
+    
+    paginator = Paginator(araclar_list, 2)  # 2 araç per sayfa
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    if request.method == 'POST':
+        form = AracForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_arac_liste')
+    else:
+        form = AracForm()
+    
+    return render(request, 'admin_arac_liste.html', {'araclar': page_obj, 'form': form})
+
+def arac_ekle(request):
+    if request.method == 'POST':
+        form = AracForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('arac_listesi')
+    else:
+        form = AracForm()
+    return render(request, 'arac_ekle.html', {'form': form})
+
+def arac_sil(request, arac_id):
+    arac = get_object_or_404(Arac, id=arac_id)
+    if request.method == 'POST':
+        arac.delete()
+        return redirect('arac_listesi')
+    return render(request, 'arac_sil.html', {'arac': arac})
+
+def admin_arac_sil(request, arac_id):
+    arac = get_object_or_404(Arac, id=arac_id)
+    if request.method == 'POST':
+        arac.delete()
+        return redirect('admin_arac_liste')
+    return render(request, 'admin_arac_liste.html', {'arac': arac})
+
+
+def arac_guncelle(request, arac_id):
+
+    arac = get_object_or_404(Arac, id=arac_id)
+    if request.method == 'POST':
+        form = AracForm(request.POST, request.FILES, instance=arac)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('admin_arac_liste'))
+    else:
+        form = AracForm(instance=arac)
+    
+    return render(request, 'arac_guncelle.html', {'form': form})
+
+def hakkımızda(request):
+    return render(request, 'hakkımızda.html')
